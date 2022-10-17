25cd051a499713879e9d88e4ab3c700a8ef1d287
Miquel Rossello <mkpuig@gmail.com>
2022-10-17 14:58:28 +0200

    fix gitignore

---
+++ dist/brfc.esm.js
@@ -0,0 +1,12 @@
+import { Hash } from 'bsv';
+
+const brfc = (title, authors, version) => {
+  const autorString = authors.join(', ').trim();
+  const stringToHash = [title.trim() + autorString + (version.toString() || '')].join('').trim();
+  let hash = Hash.sha256Sha256(Buffer.from(stringToHash));
+  hash = hash.reverse();
+  return hash.toString('hex').substring(0, 12);
+};
+
+export { brfc };
+//# sourceMappingURL=brfc.esm.js.map
